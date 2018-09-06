5475c190f2d992559b618b43cabea102b48bb721
Seryazi-Andela <phillip.seryazi@andela.com>
2018-09-06 19:01:46 +0300

    [Feature #159951821] adding functionality to edit user profiles

---
+++ src/components/SocialMediaSharing/SharingArticle.js
@@ -0,0 +1,70 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import {
+  FacebookShareButton,
+  TwitterShareButton,
+  EmailShareButton,
+  FacebookIcon,
+  TwitterIcon,
+  EmailIcon,
+} from 'react-share';
+
+import classes from '../../CSS/SharingArticle.css';
+
+class SharingArticleComponent extends Component {
+  render() {
+    const { title, url } = this.props;
+    const shareUrl = url;
+
+    const buttonContainer = ['Demo__some-network'];
+    const individualButton = ['Demo__some-network__share-button'];
+    return (
+      <div className={classes.Demo__container}>
+        <span className={classes.shareText}>Share:</span>
+        <div className={buttonContainer}>
+          <FacebookShareButton
+            url={shareUrl}
+            quote={title}
+            className={individualButton}
+          >
+            <FacebookIcon size={32} round />
+          </FacebookShareButton>
+        </div>
+
+        <div className={buttonContainer}>
+          <TwitterShareButton
+            url={shareUrl}
+            title={title}
+            className={individualButton}
+          >
+            <TwitterIcon size={32} round />
+          </TwitterShareButton>
+        </div>
+
+        <div className={buttonContainer}>
+          <EmailShareButton
+            url={shareUrl}
+            subject={title}
+            body={shareUrl}
+            className={individualButton}
+          >
+            <EmailIcon size={32} round />
+          </EmailShareButton>
+        </div>
+      </div>
+    );
+  }
+}
+
+
+SharingArticleComponent.propTypes = {
+  title: PropTypes.string,
+  url: PropTypes.string,
+};
+
+SharingArticleComponent.defaultProps = {
+  title: '',
+  url: '',
+};
+
+export default SharingArticleComponent;
---
+++ src/tests/components/SharingArticle.test.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import SharingArticleComponent from '../../components/SocialMediaSharing/SharingArticle';
+
+describe('SharingArticleComponent', () => {
+  const wrapper = shallow(<SharingArticleComponent />);
+  it('it renders share text', async () => {
+    expect(wrapper.find('span').text()).toEqual('Share:');
+  });
+});
